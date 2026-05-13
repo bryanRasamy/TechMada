9ca2dc27f60ec6b74db105ef2c4585193243d90e
AbcAjerisAbc <hasinarajerison2006@gmail.com>
2026-05-13 11:53:05 +0300

    GestionUser

--- app/Controllers/GestionUser.php
+++ app/Controllers/GestionUser.php
@@ -45,7 +45,7 @@ class GestionUser extends BaseController{
                 'actif' => $employer['actif']
             ]);
 
-            return redirect()->to('/employer/MesDemandes')->with('success', 'Connexion réussie.');
+            return redirect()->to('/employe/MesDemandes')->with('success', 'Connexion réussie.');
         }
 
         $errorMsg = 'Email ou mot de passe incorrect.';
